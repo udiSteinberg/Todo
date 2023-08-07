3fb97fc19ca3a586713cb4b929f9ee39d8278660
udi steinberg <udiSteinberg97@gmail.com>
2023-08-07 14:34:00 +0300

    Added js of adding tasks

--- src/components/InputTask.jsx
+++ src/components/InputTask.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-export const InputTask = () => {
-    let date = new Date;
+export const InputTask = (props) => {
+    let date = new Date();
+    let nameRef = useRef();
+    let timeRef = useRef();
+
+    const addTask = () => {
+        let toDoItem = {
+            id:Date.now(),
+            name:nameRef.current.value,
+            time:timeRef.current.value
+        }
+        
+        props.addTaskToArr(toDoItem);
+    }
 
     return (
         <div>
             <h3>Task Name:</h3>
-            <input type="text" placeholder='Enter task' className='form-control' />
+            <input
+                type="text"
+                className='form-control'
+                placeholder='Enter task'
+                ref={nameRef} />
             <h3>Time:</h3>
-            <input type="time" defaultValue={`${date.getHours()}:${date.getMinutes()}`} className='form-control' />
+            <input
+                type="time"
+                className='form-control'
+                defaultValue={`${date.getHours()}:${date.getMinutes()}`}
+                ref={timeRef} />
             <div className="my-3 text-center">
-                <button className='btn btn-success me-2'>Add Task</button>
+                <button
+                    className='btn btn-success me-2'
+                    onClick={addTask}
+                >Add Task</button>
                 <button className='btn btn-danger'>Reset Tasks</button>
             </div>
         </div>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { InputTask } from '../components/InputTask'
 import { TaskList } from '../components/TaskList'
 
 export const MainPage = () => {
+    let [tasks_ar, setTasks_ar] = useState([]);
+
+    const addTaskToArr = (_taskItem)=>{
+        setTasks_ar([...tasks_ar, _taskItem]);
+    }
+
     return (
         <div className='container'>
             <h1 className='display-4 text-center'>ToDo App</h1>
             <div className='col-lg-6 p-3 mx-auto'>
-                <InputTask />
+                <InputTask addTaskToArr = {addTaskToArr}/>
                 <TaskList />
             </div>
         </div>
