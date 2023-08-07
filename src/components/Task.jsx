f37b6deea43f322085d71b3084330b8b98459735
udi steinberg <udiSteinberg97@gmail.com>
2023-08-07 13:39:50 +0300

    Addead component basic style and how app gonna look

---
+++ src/components/Task.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Task = () => {
+  return (
+    <div className='shadow my-2 p-2'>
+        <button className='btn btn-info float-end'>Done</button>
+        <h4>Go Home - 15:35</h4>
+    </div>
+  )
+}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,11 +3,13 @@ import { InputTask } from '../components/InputTask'
 import { TaskList } from '../components/TaskList'
 
 export const MainPage = () => {
-  return (
-    <div className='container'>
-        <h1>ToDo App</h1>
-        <InputTask/>
-        <TaskList/>
-    </div>
-  )
+    return (
+        <div className='container'>
+            <h1 className='display-4 text-center'>ToDo App</h1>
+            <div className='col-lg-6 p-3 mx-auto'>
+                <InputTask />
+                <TaskList />
+            </div>
+        </div>
+    )
 }
