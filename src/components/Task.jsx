f6d776382c9fdebfc5838a6dcb2998c522307d12
udi steinberg <udiSteinberg97@gmail.com>
2023-08-07 15:15:01 +0300

    Added js of shwing tasks an option to reset list

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-export const Task = () => {
-  return (
-    <div className='shadow my-2 p-2'>
-        <button className='btn btn-info float-end'>Done</button>
-        <h4>Go Home - 15:35</h4>
-    </div>
-  )
+export const Task = (props) => {
+    let { name, time, id } = props.item;
+
+    return (
+        <div className='shadow my-2 p-2'>
+            <button className='btn btn-info float-end'>Done</button>
+            <h4>{name} - {time}</h4>
+        </div>
+    )
 }
